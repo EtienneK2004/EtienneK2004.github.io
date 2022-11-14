52ebcb74ed5ca418f9606198758e88df4c8bfac1
ATN04 <95452323+EtienneK2004@users.noreply.github.com>
2022-11-14 14:59:35 +0100

    Working langage system

---
+++ langage_db.js
@@ -0,0 +1,52 @@
+db = {};
+db["title-experience"] = {
+    en: "Experience",
+    fr: "Expérience"
+};
+
+db["title-stage2"] = {
+    en: "Second AirFrance Internship",
+    fr: "Deuxième stage à AirFrance"
+};
+
+db["p1-stage2"] = {
+    en: "Date: June 2019",
+    fr: "Date: Juin 2019"
+};
+
+db["p2-stage2"] = {
+    en: "Carried out within a team of developers working on the backend of AirFrance's website, more specifically handeling requests for options on a flight and travel agencies.",
+    fr: "Stage effectué au sein d'une équipe de développeurs travaillant sur le backend du site internet d'AirFrance, plus spécifiquement la gestion de requêtes des agences de voyages et les options de vol."
+};
+
+db["p3-stage2"] = {
+    en: "Learned about: Requests handeling, SCRUM methodology, test cases and server perfomances.",
+    fr: "Compétences acquises: Gestion de requêtes, méthodologie SCRUM, cas de tests et perfomances de serveur."
+};
+db["p4-stage2"] = {
+    en: "Could visit: The developers' premises and control room of AirFrance's servers.",
+    fr: "J'ai pu visiter: Les locaux des devs et la salle de contrôle des serveurs AirFrance."
+};
+
+
+db["title-stage1"] = {
+    en: "AirFrance Internship 1",
+    fr: "Premier stage à AirFrance"
+};
+
+db["nav-home"] = {
+    en: "Home",
+    fr: "Accueil"
+};
+db["nav-experience"] = {
+    en: "Experience",
+    fr: "Expérience"
+};
+db["nav-hobby"] = {
+    en: "Hobbys",
+    fr: "Loisirs"
+};
+db["nav-project"] = {
+    en: "Projects",
+    fr: "Projets"
+};
